eabb6195f3e4d6934a9655ce3f559f89dc8f5a5c
bpluta <bartekpluta@me.com>
2020-11-16 01:08:38 +0100

    Modified run script for Magic Padding Oracle challenge

--- run.py
+++ run.py
@@ -1,18 +1,28 @@
 import decoder
 import encoder
 
+import pwn
+import utils
+
+pwn.context.log_level = 'error'
+
 def is_valid(message):
-    # This method should send message to our oracle
-    # Basing on given response it should return True if the message has been decrypted correctly
-    # or False if it could not be decoded (or returned invalid padding message)
-    raise Exception("Unimplemented oracle!")
+     connection = pwn.remote('2018shell.picoctf.com', 24933)
+     connection.recvuntil('What is your cookie?\n')
+     connection.sendline(message)
+     response = utils.get_string(connection.recvall())
+     is_valid = False
+     if response.find('invalid padding') == -1:
+         is_valid = True
+     connection.close()
+     return is_valid
 
 # Decryption
-cipher = "" # cipher we want to decrypt
+cipher = "5468697320697320616e204956343536d6ca0a2883280762915414c54e97df1b40871b72f45ec7f9510a080095436d514129e137aaac86a0f7fa8bd3d250b9d1df35b668fcb93f00bb06692560a3fed8a3b523d385f1477b6daac14ff2416c67"
 decoded_message = decoder.decode_cipher(cipher, is_valid)
 print(decoded_message)
 
 # Encryption
-message = "" # message we want to encrypt
+message = '{"username": "hacker", "expires": "9999-12-31", "is_admin": "true"}'
 encoded_message = encoder.encode_message(message, is_valid)
 print(encoded_message)
